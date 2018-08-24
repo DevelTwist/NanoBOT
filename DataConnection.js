b469d9490f5b53d58f10d2eca485d5c9c8d7390a
DevelTwist <luisf55horn@gmail.com>
2018-08-24 19:36:50 -0400

    Agregado dataConnection

---
+++ DataConnection.js
@@ -0,0 +1,15 @@
+
+		module.exports = {
+
+			nameApi: function(){
+				return "node-telegram-bot-api";
+			},
+			token: function(){
+				return "679471073:AAHS67sW1BEeN5DH1qHFmI92bj6CT-8ork4";
+			}
+
+		}
+
+
+
+
